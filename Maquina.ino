16bd2d5bd3d5d45dff34544d10fbafdd69f1832f
Ronny Zárate <31499007+RonnyZF@users.noreply.github.com>
2018-05-20 16:31:46 -0600

    Update Maquina.ino

--- Maquina.ino
+++ Maquina.ino
@@ -561,7 +561,7 @@ void transf_datos{
   
     case segundo_estado:
     {
-      delay((4*B) ; // espera un tiempo aleatorio
+      delay((rand() % 9 + 2)*B) ; // espera un tiempo aleatorio
       if (RTS_st != 0 or CTS_st !=0)
       {
         if (contador_s == 3)
